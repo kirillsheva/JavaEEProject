c4012d6dcd1028607c1699a9af305d722e21706f
Kyrylo <49348101+kirillsheva@users.noreply.github.com>
2021-04-16 20:37:22 +0300

    Service done

---
+++ backend/backend/src/main/java/com/example/demo/service/ProductServiceImpl.java
@@ -0,0 +1,308 @@
+//IT18125412 T. M. D. D. Thalakumbura
+
+package com.example.demo.service;
+
+import com.example.demo.model.Product;
+import com.example.demo.repo.ProductRepo;
+import com.example.demo.repo.UserRepo;
+import com.example.demo.requestBodies.Comment;
+import com.example.demo.requestBodies.CommentReq;
+import com.example.demo.requestBodies.OwnerReq;
+import com.example.demo.requestBodies.ProductRequest;
+import com.example.demo.requestBodies.ProductUpdateRequest;
+import com.example.demo.requestBodies.RatingReq;
+import com.example.demo.requestBodies.categoryreq;
+import com.example.demo.requestBodies.discountReq;
+import com.example.demo.requestBodies.productid;
+
+import org.bson.BsonBinarySubType;
+import org.bson.types.Binary;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Service
+public class ProductServiceImpl  {
+	
+	
+	@Autowired
+	ProductRepo repo;
+	
+	@Autowired
+	UserRepo urepo;
+	
+	
+	public String addProduct(ProductRequest req) {
+		
+		Product p=repo.save(new Product(req.pname, req.pdescription, req.pdiscount, req.pprice, req.powner,req.pcategory));
+	
+		if(p==null) {
+			return "error";
+		}
+		System.out.println(p.getId());
+		return p.getId();
+		
+	}
+	
+	public void updateProduct(ProductUpdateRequest request) {
+		
+		List<Product> list = repo.findAll();
+	      
+	      for(Product product: list) {
+	    	  
+	    	  if(product.getId().equals(request.id)) {
+	    		  
+	    		  product.setPname(request.pname);
+	    		  product.setPdescription(request.pdescription);
+	    		  product.setPdiscount(request.pdiscount);
+	    		  product.setPprice(request.pprice);
+	    		  product.setPcategory(request.pcategory);
+	 
+	    		  repo.save(product);
+	    		  
+	    	  }
+	      }
+	      System.out.println("Product updated..");
+		
+	}
+	
+	public List<Product> getAllProducts(){
+		
+		return repo.findAll();
+	}
+	
+	
+	public void addComment(CommentReq req) {
+		
+	      List<Product> list = repo.findAll();
+	      
+	      for(Product product: list) {
+	    	  
+	    	  if(product.getId().equals(req.productid)) {
+	    		  
+	    		  product.setPcomments(new Comment(req.username, req.comment));
+	    		  
+	    		  repo.save(product);
+	    		  
+	    	  }
+	      }
+		
+	}
+	
+	
+	public void addRating(RatingReq req) {
+		
+	      List<Product> list = repo.findAll();
+	      
+	      for(Product product: list) {
+	    	  
+	    	  
+	    	  if(product.getId().equals(req.productid)) {
+	    		  
+	    		  System.out.println(product.getId());
+	    		  
+	    		  product.setPcomments(new Double(req.rating));
+	    		 
+	    		  
+	    		  repo.save(product);
+	    		  
+	    	  }
+	      }
+		
+	}
+	
+	
+	public double getRating(String pid) {
+		
+		List<Product> list= repo.findAll();
+		
+		   for(Product product: list) {
+		    	  
+		    	  if(product.getId().equals(pid)) {
+		    		  
+		    		  double total=0;
+		    		  
+		    		  for(double x: product.getPratings()) {
+		    			  
+		    			  total=total+x;
+		    			  
+		    		  }
+		    		  
+		    		  return total/product.getPratings().size();
+		    		  
+		    	  }
+		      }
+		return 0;
+		
+	}
+	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	public void setDiscount(discountReq req) {
+		
+	    List<Product> list = repo.findAll();
+	      
+	      for(Product product: list) {
+	    	  
+	    	  if(product.getId().equals(req.productid)) {
+	    		  
+	    		  product.setPdiscount(req.discount);
+	    		  
+	    		  repo.save(product);
+	    		  
+	    	  }
+	      }
+		
+	}
+	
+	public List<Product> getProductsById(productid pid){
+		List<Product> list=repo.findAll();
+		List<Product> oneProductList= new ArrayList<Product>();
+		
+		for(Product product: list) {
+			
+			if(product.getId().equals(pid.pid)) {
+				
+				oneProductList.add(product);
+				
+			}
+		}
+		return oneProductList;
+		
+	}
+	
+	public List<Product> getProductsByOwner(OwnerReq req){
+		
+		List<Product> list=repo.findAll();
+		List<Product> sortedlist= new ArrayList<Product>();
+		
+		
+		for(Product product: list) {
+			
+			if(product.getPowner().equals(req.username)) {
+				
+				sortedlist.add(product);
+			}
+		}
+		
+		
+		return sortedlist;
+		
+	}
+	
+	public String uploadImages(MultipartFile file, String productid) throws IOException {
+		
+		
+		List<Product> list=repo.findAll();
+		
+		for(Product product: list) {
+			
+			if(product.getId().equals(productid)) {
+				
+				product.setPimages(new Binary(BsonBinarySubType.BINARY,file.getBytes()));
+				
+				repo.save(product);	
+			}
+		}
+		return "done";
+		
+		
+	}
+	
+	
+	public List<Product> getproductBycategory(categoryreq req){
+		
+		List<Product> list= repo.findAll();
+		List<Product> sortedList= new ArrayList<>();
+		
+		for(Product product: list) {
+			
+			if(product.getPcategory().equals(req.category)) {
+				sortedList.add(product);
+				
+			}
+		}
+		
+		return sortedList;
+		
+	}
+	
+	public void removeProduct(String id) {
+		
+		
+		List<Product> list= repo.findAll();
+		
+		for(Product product: list) {
+			
+			if(product.getId().equals(id)) {
+				
+				repo.delete(product);
+			}
+			
+		}
+		
+	}
+	
+	public List<Product> searchProducts(String query){
+		
+		List<Product> list= repo.findAll();
+		List<Product> serchedlist= new ArrayList<>();
+		
+		String regex = query.toLowerCase();
+		
+		Pattern pattern = Pattern.compile(regex);
+		
+		for(Product product:list) {
+			
+			Matcher matcher1 =pattern.matcher(product.getPname().toLowerCase());
+			Matcher matecher2= pattern.matcher(product.getPdescription().toLowerCase());
+			
+			if(matcher1.find()|| matecher2.find()) {
+				
+				serchedlist.add(product);
+				
+			}
+			
+			
+			
+		}
+		
+		return serchedlist;
+		
+		
+		
+	}
+	
+
+	public List<Comment> getComments(String pid){
+		
+		List<Product> list= repo.findAll();
+		
+		for(Product product:list) {
+			
+			if(product.getId().equals(pid)) {
+				
+				
+				return product.getComments();
+			}
+		}
+		return new ArrayList<>();
+	}
+
+  
+}
---
+++ backend/backend/src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,434 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.mailUtil.MailUtil;
+import com.example.demo.mailUtil.RandomString;
+import com.example.demo.model.Order;
+import com.example.demo.model.Product;
+import com.example.demo.model.User;
+import com.example.demo.repo.ProductRepo;
+import com.example.demo.repo.UserRepo;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	UserRepo repo;
+	
+	@Autowired
+	ProductRepo prepo;
+	
+	public User findUserByUsername(String username) {
+		
+		try {
+		List<User> list= repo.findByUsername(username);
+		
+		for(User x: list) {
+			
+			if(x.getUsername().equals(username)) {
+				
+				return x;
+			}
+			else {return null;}
+			
+		}
+		
+		}catch (Exception e) {
+		    return null;
+		}
+		
+		return null;
+	}
+	
+	public User findUserByEmail(String email) {
+			
+			try {
+			List<User> list= repo.findByEmail(email);
+			
+			for(User x: list) {
+				
+				if(x.getEmail().equals(email)) {
+					System.out.println("email user found");
+					return x;
+				}
+				else {
+					return null;
+					}
+				
+			}
+			
+			}catch (Exception e) {
+			    return null;
+			}
+			
+			return null;
+		}
+	
+	
+	public boolean AddUser(String username, String fname, String lname, String address, String cnumber, String usertype,
+			String bday, String password, String email) {
+		
+		try {
+			
+		     User user= findUserByUsername(username);
+		     
+		     if(user!=null) {
+		    	 return false;
+		     }
+			
+			if(usertype.toLowerCase().equals("manager")) {
+				String randomPassword = RandomString.getRandomString();
+				User newManager = new User(username, fname, lname, address, cnumber, usertype, bday, hashPassword(randomPassword), email);
+				repo.save(newManager);
+				MailUtil.sendEmail(email, username, randomPassword);
+			}else {
+				User newUser = new User(username, fname, lname, address, cnumber, usertype, bday, hashPassword(password), email);
+				repo.save(newUser);
+			}
+			
+			return true;
+			
+		}catch (Exception e) {
+			return false;
+		}
+		
+	}
+	
+	
+	public String Login(String username,String password) {
+		
+		String uname = null;
+		
+		try {
+			User user= findUserByUsername(username);
+			User user2 = findUserByEmail(username);
+			
+			if(user==null && user2 == null) {
+				
+				System.out.println("user null");
+				
+			}
+			if (user != null) {
+				if (new BCrypt().checkpw(password, user.getPassword())) {
+					uname = user.getUsername();
+				}	
+			}
+			if (user2 != null) {
+				System.out.println(user2.getPassword()+ "      "+ password + "    " + hashPassword(password) );
+				if (new BCrypt().checkpw(password, user2.getPassword())) {
+					uname = user2.getUsername();
+				}	
+			}
+			
+			
+		}catch (Exception e) {}
+		
+		System.out.println(uname);
+		return uname;
+		
+		
+		
+	}
+	
+	public List<User> getAllUsers(){
+		
+		return repo.findAll();
+	}
+	
+	public List<Order> getCart(String username){
+		
+		List<User> list=repo.findAll();
+		
+		for(User user:list){
+			
+			if(user.getUsername().equals(username)) {
+				return user.getCart();
+			}
+			
+		}
+		
+		return new ArrayList<Order>();
+	
+	}
+	
+	public List<Product> getWishList(String username){
+		
+		
+	List<User> list=repo.findAll();
+		
+		for(User user:list){
+			
+			if(user.getUsername().equals(username)) {
+				return user.getWishlist();
+			}
+			
+		}
+		
+		return new ArrayList<Product>();
+
+}
+
+	
+	public List<Order> removeFromCart(String username,int index){
+		
+		List<User> list= repo.findAll();
+		
+		for(User user:list) {
+			
+			if(user.getUsername().equals(username)) {
+				
+				user.removeFromCart(index);
+				
+				repo.save(user);
+				
+				return user.getCart();
+			}
+			
+			
+		}
+		
+		return null;
+	}
+	
+	
+	public void addtoWishList(String username,String pid) {
+		
+		List<User> list=repo.findAll();
+		
+		for(User user:list) {
+			
+			if(user.getUsername().equals(username)) {
+				
+				List<Product> wlist=user.getWishlist();
+				
+				for(Product product:wlist) {
+					
+					if(product.getId().equals(pid)) {
+						
+						return;
+					}
+					
+				}
+				
+
+				List<Product> plist=prepo.findAll();
+				
+				for(Product product: plist) {
+					
+					if(product.getId().equals(pid)) {
+						
+						user.setWishlist(product);
+						repo.save(user);
+						
+					}
+				}
+				
+			}
+		}
+		
+		
+	}
+	
+	
+	public List<Product> removeFromWishList(String username,String pid){
+		
+		List<User> list= repo.findAll();
+		
+		for(User user:list) {
+			if(user.getUsername().equals(username)) {
+				
+				
+				user.removefromWishList(pid);
+				
+				repo.save(user);
+				
+				return user.getWishlist();
+			}
+		}
+		
+		return new ArrayList<Product>();
+	}
+
+	
+	public String changePassword(String username, String oldPassword, String newPassword) {
+		
+		String msg = null;
+		List<User> list=repo.findAll();
+		
+		for(User user : list){
+			
+			if(user.getUsername().equals(username)) {		
+				if(new BCrypt().checkpw(oldPassword, user.getPassword())) {
+					user.setPassword(hashPassword(newPassword));
+					repo.save(user);
+					msg = "success";
+					System.out.println("password changed...");
+				}else {
+					msg = "faliure";
+					System.out.println("password changed error....");
+				}
+			}
+			
+		}
+		
+		return msg;
+		
+	}
+	
+public boolean updateProfile(String username, String fname, String lname, String email, String cnumber, String address, String bday) {
+		
+		List<User> list=repo.findAll();
+		
+		for(User user : list){
+			
+			if(user.getUsername().equals(username)) {		
+				user.setFname(fname);
+				user.setLname(lname);
+				user.setEmail(email);
+				user.setCnumber(cnumber);
+				user.setAddress(address);
+				user.setBday(bday);
+				
+				repo.save(user);
+				
+				return true;
+			}
+			
+		}
+		return false;
+	}
+	
+	private String hashPassword(String plainTextPassword){
+		return BCrypt.hashpw(plainTextPassword, BCrypt.gensalt());
+	}
+	
+
+	public void setOrder(String username, String pid, int quan) {
+
+
+		List<User> list=repo.findAll();
+
+		for(User user:list){
+
+			if(user.getUsername().equals(username)) {
+
+
+
+				List<Product> plist= prepo.findAll();
+
+				for(Product product:plist) {
+
+
+					if(product.getId().equals(pid)) {
+
+
+						user.setCart(new Order(product, quan));
+						repo.save(user);
+
+					}
+				}
+			}
+
+		}
+
+
+	}
+
+
+	public void purchaseItem(String username, String pid, int quantity) {
+
+		List<User> list = repo.findAll();
+
+		for (User user : list) {
+
+			if (user.getUsername().equals(username)) {
+
+				List<Product> wlist = user.getPurchased_items();
+
+				for (Product product : wlist) {
+
+					if (product.getId().equals(pid)) {
+
+						return;
+					}
+
+				}
+
+
+				List<Product> plist = prepo.findAll();
+
+				for (Product product : plist) {
+
+					if (product.getId().equals(pid)) {
+
+						user.setPurchased_items(product);
+						repo.save(user);
+
+					}
+				}
+
+			}
+		}
+	}
+
+		public List<Product> getPurchasedItems (String username){
+
+			List<User> list = repo.findAll();
+
+			for (User user : list) {
+
+				if (user.getUsername().equals(username)) {
+					return user.getPurchased_items();
+				}
+
+			}
+
+			return new ArrayList<Product>();
+
+		}
+		
+		public void removeUser(String username) {
+			
+			
+			List<User> list= repo.findAll();
+			
+			for(User user: list) {
+				
+				if(user.getUsername().equals(username)) {
+					
+					repo.delete(user);
+				}
+				
+			}
+			
+		}
+		
+		
+		public void resetPassword(String username, String password) {
+			
+			
+			List<User> list= repo.findAll();
+			
+			for(User user: list) {
+				
+				if(user.getUsername().equals(username)) {
+					
+					user.setPassword(hashPassword(password));
+					repo.save(user);
+				}
+				
+			}
+			
+		}
+		
+		
+		
+		
+		
+	}
