ed7f65d65656bc9df78baa886d0f3a2e39461668
Kyrylo <49348101+kirillsheva@users.noreply.github.com>
2021-04-16 20:35:30 +0300

    Services done

---
+++ service/CategoryService.java
@@ -0,0 +1,56 @@
+package com.example.demo.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Category;
+import com.example.demo.repo.CategoryRepo;
+import com.example.demo.requestBodies.CategoryRequest;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	CategoryRepo repo;
+	
+	public void addCategory(CategoryRequest request) {
+		
+		repo.save(new Category(request.cname));
+	}
+
+	
+	public List<String> getAllCategories(){
+		
+		List<String> namelist= new ArrayList<String>();
+		List<Category> list= repo.findAll();
+		
+		for(Category cat: list) {
+			
+			namelist.add(cat.getCname());
+		}
+		
+		return namelist;
+		
+	} 
+	
+	public void removeCategory(String name) {
+		
+		List<Category> list= repo.findAll();
+		
+		for(Category category: list) {
+			
+			if(category.getCname().equals(name)) {
+				
+				repo.delete(category);
+			}
+			
+		}
+		
+	}
+	
+	
+	
+}
