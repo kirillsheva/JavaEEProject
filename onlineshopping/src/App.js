2cba84c669a5b15636cac6d4747cd13184b3b8ab
Meredan <meredan.dn@gmail.com>
2021-04-17 15:20:10 +0300

    Minor fixes final

--- onlineshopping/src/App.js
+++ onlineshopping/src/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 
 import './App.css';
 import Test from "./Components/TestPage/Test";
-
+import ComplexNavigationBar from "./Components/Common/ComplexNavigationBar/ComplexNavigationBar";
+import LoggedHome from "./Components/LoggeHome/LoggedHome";
+import Profile from "./Profile/Profile";
+import Chat from "./Components/Test/Test";
+import AdminHome from "./Components/Admin Panel/AdminHome";
+import Product from "./Components/Product/Product";
+import ProductsData from "./Components/Store Manager/ProductsData";
+import DetailForm from "./Components/PaymentGateway/DetailForm";
+import NonLoggedHome from "./Components/NonLoggedHome/NonLoggedHome";
+import CheckoutForm from "./Components/PaymentGateway/CheckoutForm";
+import ProductsTable from "./Components/Admin Panel/Tables/ProductsTable";
 
 
 function App() {
