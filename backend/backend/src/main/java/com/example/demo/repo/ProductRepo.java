7d0287dbc4c324ed584fd07d359f2a62b82877f4
Kyrylo <49348101+kirillsheva@users.noreply.github.com>
2021-04-18 22:22:31 +0300

    Fixes

--- backend/backend/src/main/java/com/example/demo/repo/ProductRepo.java
+++ backend/backend/src/main/java/com/example/demo/repo/ProductRepo.java
@@ -1,4 +1,3 @@
-//IT18125412 T. M. D. D. Thalakumbura
 package com.example.demo.repo;
 
 import com.example.demo.model.Product;
@@ -11,16 +10,4 @@ import java.util.List;
 @Repository
 public interface ProductRepo extends MongoRepository<Product, String> {
 
-
-
-
-    
-//    Product getProductById(Product productId);
-//    Product addProduct(Product product);
-//    void updateProduct(Product product);
-//    List<Product> getAllProducts();
-//    Product getProductById(@Param("ProductId") Product productId);
-//    Product addProduct(@Param("ProductId") Product product);
-//    void updateProduct(@Param("ProductId") Product product);
-//    void deleteProduct(@Param("ProductId") Product product);
 }
