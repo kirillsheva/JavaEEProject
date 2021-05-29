9859eaec62506125262447d1ed2515de2be88b6c
Meredan <meredan.dn@gmail.com>
2021-05-29 18:44:08 +0300

    Update not working queue

--- backend/backend/src/main/java/com/example/demo/controller/ChatController.java
+++ backend/backend/src/main/java/com/example/demo/controller/ChatController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,27 +14,29 @@ import com.example.demo.service.ChatService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
+
 public class ChatController {
 
-	
-	@Autowired
-	ChatService service;
-	
-	
-	@RequestMapping("/allmessages")
-     public AllChats getAllChats() {
-    	 
-    	 return service.GetAllMessages();
-     }
-	
-	
-	@RequestMapping("/sendmessage")
-	public Message sendMessage(@RequestBody ChatRequst request) {
-		
-		return service.addMessage(request.from, request.msg);
+
+	@Autowired private SimpMessagingTemplate messagingTemplate;
+	@Autowired private ChatMessageService chatMessageService;
+	@Autowired private ChatRoomService chatRoomService;
+
+
+	@MessageMapping("/allMessages")
+	public void processMessage(@Payload ChatMessage chatMessage) {
+		var chatId = chatRoomService
+				.getChatId(chatMessage.getSenderId(), chatMessage.getRecipientId(), true);
+		chatMessage.setChatId(chatId.get());
+
+		ChatMessage saved = chatMessageService.save(chatMessage);
+
+		messagingTemplate.convertAndSendToUser(
+				chatMessage.getRecipientId(),"/queue/messages",
+				new ChatNotification(
+						saved.getId(),
+						saved.getSenderId(),
+						saved.getSenderName()));
 	}
-	
-	
-	
 	
 }
