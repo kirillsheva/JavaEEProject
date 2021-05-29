d83040eec2ac58797e109b501dfdd52cbd1a21aa
Meredan <meredan.dn@gmail.com>
2021-05-29 18:38:46 +0300

    Config update

--- backend/backend/src/main/java/com/example/demo/Config/WebSocketConfig.java
+++ backend/backend/src/main/java/com/example/demo/Config/WebSocketConfig.java
@@ -1,8 +1,15 @@
 package com.example.demo.Config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.messaging.converter.DefaultContentTypeResolver;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+import java.util.List;
 
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
@@ -18,6 +25,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void configureMessageBroker(MessageBrokerRegistry registry) {
 	
 		registry.setApplicationDestinationPrefixes("/app").enableSimpleBroker("/topic");
+	}
+
+	@Override
+	public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
+		DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
+		resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
+		MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+		converter.setObjectMapper(new ObjectMapper());
+		converter.setContentTypeResolver(resolver);
+		messageConverters.add(converter);
+		return false;
 	}
 
 }
